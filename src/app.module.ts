a71b02d237e3583a79180bbac772215e9a2a5127
KauanAmorim <kauanamorim88@gmail.com>
2024-08-26 21:24:11 -0300

    feat: importa modulo usuarioPesquisaModule em appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { UsuarioModule } from './modules/usuario/usuario.module';
 import { PesquisaModule } from './modules/pesquisa/pesquisa.module';
 import { PerguntaModule } from './modules/pergunta/pergunta.module';
 import { RespostaModule } from './modules/resposta/resposta.module';
-import { PerguntaRespostaModule } from './modules/pergunta-resposta/pergunta-resposta.module';
+import { PerguntaRespostaModule } from './modules/pergunta-resposta/perguntaResposta.module';
+import { UsuarioPesquisaModule } from './modules/usuario-pesquisa/usuarioPesquisa.module';
 
 @Module({
   imports: [
     UsuarioModule,
+    UsuarioPesquisaModule,
     PesquisaModule,
     PerguntaModule,
     RespostaModule,
