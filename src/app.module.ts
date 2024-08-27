0c91c6cf3745812e44c9fbacee09b60734b2b3ac
KauanAmorim <kauanamorim88@gmail.com>
2024-08-26 22:15:55 -0300

    feat: ativando validação de dtos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { UsuarioModule } from './modules/usuario/usuario.module';
 import { PesquisaModule } from './modules/pesquisa/pesquisa.module';
 import { PerguntaModule } from './modules/pergunta/pergunta.module';
@@ -6,6 +6,7 @@ import { RespostaModule } from './modules/resposta/resposta.module';
 import { PerguntaRespostaModule } from './modules/pergunta-resposta/perguntaResposta.module';
 import { UsuarioPesquisaModule } from './modules/usuario-pesquisa/usuarioPesquisa.module';
 import { CriarPesquisaCompleta } from './modules/pesquisa-completa/services/criarPesquisaCompleta.service';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -18,6 +19,11 @@ import { CriarPesquisaCompleta } from './modules/pesquisa-completa/services/cria
     CriarPesquisaCompleta,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
+  ],
 })
 export class AppModule {}
